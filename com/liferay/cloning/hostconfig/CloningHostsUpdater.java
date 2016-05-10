f1cd641840c5b492ff0d2e156541e532520db258
giros <gergely.mathe@liferay.com>
2016-05-10 15:00:19 +0200

    WIP

---
+++ com/liferay/cloning/hostconfig/CloningHostsUpdater.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.cloning.hostconfig;
+
+/**
+ * @author Gergely Mathe
+ */
+ public class CloningHostsUpdater {
+
+	// Update company level virtual hosts
+	
+	// Update site level virtual hosts
+	
+	// Update remote live site configs
+	
+	// Update CDN hosts
+	
+	// Update authentication related hosts
+
+ }
